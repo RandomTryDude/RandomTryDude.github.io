a9ff6d27e71bf7856c0d9c278257b0cc5a2242ef
RandomTryDude <ctf@KaliCTF>
2023-12-12 23:21:40 +0100

    Light fix + The Team

--- js/main.js
+++ js/main.js
@@ -1,34 +1,70 @@
+const userInput = document.getElementById('userInput');
+const dummyKeyboard = document.getElementById('dummyKeyboard');
+const terminalOutput = document.getElementById('terminalOutput');
 
-  function setDarkModePreference(isDarkModeEnabled) {
-    if (isDarkModeEnabled) {
-      localStorage.setItem('preference', 'DarkMode');
-    } else {
-      localStorage.setItem('preference', 'LightMode');
-    }
+function displayCommandOutput(command, response) {
+  // Display user input
+  const commandLine = document.createElement('div');
+  commandLine.classList.add('terminal-line');
+  commandLine.innerHTML = `<span class="id">┌──Zsociety</span>@<span class="pc-name">Team</span><br><span class="directory">└─$</span> <span class="user-input">${command}</span>`;
+  terminalOutput.appendChild(commandLine);
+
+  // Display command response
+  const responseLines = response.split('\n');
+  responseLines.forEach((line) => {
+    const responseLine = document.createElement('div');
+    responseLine.classList.add('terminal-line');
+
+    // Convert URLs to clickable links
+    const lineWithLinks = line.replace(/(https?:\/\/[^\s]+)/g, function (url) {
+      if (url.includes('linkedin.com')) {
+        return `<a href="${url}" target="_blank">[LinkedIn]</a>`;
+      } else if (url.includes('discord.com')) {
+        return `<a href="${url}" target="_blank">[Discord]</a>`;
+      } else {
+        return `<a href="${url}" target="_blank">${url}</a>`;
+      }
+    });
+
+    responseLine.innerHTML = lineWithLinks;
+    terminalOutput.appendChild(responseLine);
+  });
+
+  // Scroll to the bottom
+  terminalOutput.scrollTop = terminalOutput.scrollHeight;
+}
+
+
+function updateUserInput(value) {
+  // Update user input display
+  userInput.textContent = value;
+}
+
+dummyKeyboard.addEventListener('keydown', function (e) {
+  if (e.key === 'Enter') {
+    // Handle Enter key press
+    const command = dummyKeyboard.value.trim();
+    const response = getCommandResponse(command);
+    displayCommandOutput(command, response);
+
+    // Clear input after processing
+    dummyKeyboard.value = '';
+    updateUserInput('');
   }
+});
 
-  function getDarkModePreference() {
-    return localStorage.getItem('preference');
-  }
+function getCommandResponse(command) {
+  // Command & responses
+  const commands = {
+    'help': 'Available commands: social, help, about, contact , Zsociety , todo',
+    'social': 'You can find us on https://discord.com/invite/da4fD5pf4e & https://www.linkedin.com/company/zdsociety/ \n both Links are usable and will open a new windows',
+    'about': 'Terminal-Like, Learn more about the team',
+    'contact': '==============================\nSpaceJam on the Network, Contact unreachable\n============================== \n + ................... + \n Contact Aborted \n  + ................... + ',
+    'HiddenSecret': "Can't believe you found me, Take it, you own it Zsociety{S0m3_C0mm4nd5_4r3_h1dd3n}",
+     'Zsociety': 'We are a team of enthusiasts dedicated to exploring and sharing knowledge in various domains.\n Our passion drives us to push boundaries and contribute to the ever-growing world of technology and beyond.\nJoin us on this exciting journey!',
+     'todo' : 'implement a clear command & workaround for case-sensitive content ',
+  };
 
-  function Darkmode() {
-    var element = document.body;
-    var checkbox = document.getElementById("toggle_checkbox");
-
-    // Check if dark mode is enabled in localStorage
-    var darkModePreference = getDarkModePreference();
-    if (darkModePreference === 'DarkMode') {
-      element.classList.add('dark-mode');
-      checkbox.checked = true;
-    }
-  }
-
-  function ColorMode() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-
-    // Save the dark mode state in localStorage
-    var isDarkModeEnabled = element.classList.contains("dark-mode");
-    setDarkModePreference(isDarkModeEnabled);
-  }
+  return commands[command] || `Ain't seen no ${command} in help`;
+}
 
